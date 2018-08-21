c3a027ed7d3da90d876223f12a103c526c8c85b2
Josh Walton <josh.walton@pearson.com>
2018-08-21 11:17:16 -0600

    update default server

--- dist/scripts/services/configService.9d7b4662.js
+++ dist/scripts/services/configService.9d7b4662.js
@@ -2,7 +2,7 @@
 angular.module('tableGraphExplorerApp').factory('ConfigService', ['$log', '$window', function ($log, $window) {
         var endpoint = $window.localStorage.getItem('server');
         var config = {
-            endpoint: endpoint ? endpoint : "http://localhost:8182/"
+            endpoint: endpoint ? endpoint : "ws://localhost:8182/gremlin"
         };
 
         return {
